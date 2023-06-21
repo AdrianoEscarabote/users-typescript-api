98489a71bac27c7134308ceb79f5042aafd81a60
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-06-21 15:11:02 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
